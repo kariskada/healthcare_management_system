95e8782633511966aa42f72a1d39597891ab86f8
Charles Kariuki <kariskada10177@gmail.com>
2025-05-02 19:57:33 +0300

    feat: add SpeedInsights component and update dependencies for performance monitoring

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
           >
           {children}
           <Toaster richColors position="top-center" />
+          <SpeedInsights />
           </body>
         </html>
     </ClerkProvider>
